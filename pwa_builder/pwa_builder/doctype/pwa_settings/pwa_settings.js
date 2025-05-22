691c1edc7d7045fc62c3c09cc788326b91a778d9
Nitin Sharma <nitin@fuelbuddy.in>
2025-05-22 05:41:17 +0000

    Added settings for PWA

---
+++ pwa_builder/pwa_builder/doctype/pwa_settings/pwa_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, SVNIX Solutions and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("PWA Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
